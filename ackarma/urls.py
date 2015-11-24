57bbd00e7b2706b7ba6fae5eccfebdd6f90df268
= <anthonyclever@gmail.com>
2015-11-24 13:59:30 -0500

    do some basic styling work

--- ackarma/urls.py
+++ ackarma/urls.py
@@ -7,7 +7,7 @@ from accounts.views import signup
 urlpatterns = [
     url(r'^grappelli/', include('grappelli.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', TemplateView.as_view(template_name='base.html'), name='home'),
+    url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
     url(r'^', include('django.contrib.auth.urls')),
     url(r'^signup/', signup, name='signup'),
 ]
