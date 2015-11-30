b620dad3b2a4f971d77cb5ad33665fa173a8d740
= <anthonyclever@gmail.com>
2015-11-30 11:40:50 -0500

    Update query to filter out two week old lineups

--- subscriptions/views.py
+++ subscriptions/views.py
@@ -1,5 +1,7 @@
 from django.template.response import TemplateResponse
 from django.contrib.auth.decorators import login_required
+from django.utils import timezone
+from datetime import timedelta
 from itertools import groupby
 
 from .models import LineUp, Subscription
@@ -8,13 +10,15 @@ from .models import LineUp, Subscription
 @login_required
 def dashboard(request):
     lineups_by_date = []
+    two_weeks_ago = timezone.now() - timedelta(days=14)
 
     # Products a user is subscribed to.
     products = request.user.product_set.all()
 
     # ordered lineups based on the above products.
     lineups = LineUp.objects.filter(
-        products__in=products).order_by('-date_uploaded')
+        products__in=products,
+        date_uploaded__gte=two_weeks_ago).order_by('-date_uploaded')
 
     # and her we group the lineups returned by the date they were uploaded,
     # so that we can format the output easily
