0cdaa153ffba077cfdaa1fbefc58222f510d5430
= <anthonyclever@gmail.com>
2015-11-30 12:18:19 -0500

    update query to include case where user subscribed after a lineup has already been sent out.

--- subscriptions/views.py
+++ subscriptions/views.py
@@ -1,8 +1,9 @@
 from django.template.response import TemplateResponse
 from django.contrib.auth.decorators import login_required
 from django.utils import timezone
+from django.db.models import Q
 from datetime import timedelta
-from itertools import groupby
+from itertools import groupby, chain
 
 from .models import LineUp, Subscription
 
@@ -12,17 +13,25 @@ def dashboard(request):
     lineups_by_date = []
     two_weeks_ago = timezone.now() - timedelta(days=14)
 
-    # Products a user is subscribed to.
-    products = request.user.product_set.all()
+    # Subscriptions a user has.
+    subscriptions = request.user.subscription_set.all().select_related(
+        'product')
 
-    # ordered lineups based on the above products.
-    lineups = LineUp.objects.filter(
-        products__in=products,
-        date_uploaded__gte=two_weeks_ago).order_by('-date_uploaded')
+    lineups = []
+
+    # ordered lineups based on the subscriptions.
+    for subscription in subscriptions:
+        lineups.append(LineUp.objects.filter(
+            Q(date_uploaded__gte=two_weeks_ago) &
+            Q(date_uploaded__gte=subscription.date_subscribed),
+            products__in=[subscription.product]
+        ).order_by('-date_uploaded'))
+
+    all_lineups = list(chain(*lineups))
 
     # and her we group the lineups returned by the date they were uploaded,
     # so that we can format the output easily
-    for date, group in groupby(lineups, key=lambda x: x.date_uploaded):
+    for date, group in groupby(all_lineups, key=lambda x: x.date_uploaded):
         lineups_by_date.append({
             'date': date,
             'lineups': list(group)
