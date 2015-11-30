4a8754641a8d62a23da1b3cd51ba0691e2641054
= <anthonyclever@gmail.com>
2015-11-30 16:03:49 -0500

    bring in bootstrap material design library

--- ackarma/urls.py
+++ ackarma/urls.py
@@ -8,6 +8,9 @@ urlpatterns = [
     url(r'^grappelli/', include('grappelli.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
+    url(r'^account/', TemplateView.as_view(
+        template_name='accounts/basic_information.html'),
+        name='account_info'),
     url(r'^', include('django.contrib.auth.urls')),
     url(r'^', include('subscriptions.urls')),
     url(r'^signup/', signup, name='signup'),
