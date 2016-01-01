d34801ee71c56636c8283acfdcd1214ec3a1b72b
Anthony Clever <anthonyclever@gmail.com>
2016-01-01 15:11:09 -0500

    set up a framework for testing JS locally and on circle-ci

---
+++ subscriptions/js_tests/test_js_tester.js
@@ -0,0 +1,9 @@
+describe('a simple test', function () {
+    it('should pass', function () {
+        expect(1).toBe(1);
+    });
+
+    it('should pass now', function () {
+        expect(true).toBe(false);
+    });
+});
