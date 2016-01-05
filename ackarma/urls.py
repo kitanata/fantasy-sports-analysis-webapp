0bef95d22ccf98bc344529f3f55a16d897adcea5
Anthony Clever <anthonyclever@gmail.com>
2016-01-05 13:34:01 -0500

    test subscriptions on home template

--- ackarma/urls.py
+++ ackarma/urls.py
@@ -1,15 +1,15 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.views.generic import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
 
 from accounts.views import signup
+from .views import homepage
 
 urlpatterns = [
     url(r'^grappelli/', include('grappelli.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', TemplateView.as_view(template_name='home.html'), name='home'),
+    url(r'^$', homepage, name='home'),
     url(r'^account/', include('accounts.urls')),
     url(r'^', include('django.contrib.auth.urls')),
     url(r'^', include('subscriptions.urls')),
