1f2c81251896b4c78c7eef312b2321e848e8e803
Anthony Clever <anthonyclever@gmail.com>
2016-01-21 10:25:02 -0500

    fix issue with missing import

--- accounts/views.py
+++ accounts/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
+from django.contrib import messages
 
 from .forms import (
     EmailUserCreationForm,
